f91a5d0958507024bc0494ba2180ece07c71512f
Robin-Git-Hub <crochetrobin@gmail.com>
2021-04-07 17:40:26 +0200

    Init Redux Devtools Ext

--- client/src/lib/state/store/index.js
+++ client/src/lib/state/store/index.js
@@ -2,6 +2,7 @@ import { createStore, compose, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import reducers from "../reducers";
 
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default function configureStore() {  
   return createStore(
