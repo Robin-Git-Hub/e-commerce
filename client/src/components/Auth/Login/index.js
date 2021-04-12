71a46db9d8b6dd8c450379dddf236f8eb991240b
Robin-Git-Hub <crochetrobin@gmail.com>
2021-04-12 14:50:37 +0200

    Login Auth - user -

--- client/src/components/Auth/Login/index.js
+++ client/src/components/Auth/Login/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Link } from "react-router-dom";
 import * as Input from '../Input'
+import { useFormValidation } from "../../../lib/hooks/useFormValidation"
 
 const Alert = ({ isVisible }) => (
 	isVisible &&
@@ -19,28 +20,44 @@ const defaultValues = {
 	email: 'crochetrobin@gmail.com' ,
 	password: '' ,
 }
-const Login = () => { 
+
+const Login = ({history}) => { 
+
+  const {formValues, validate, register, handleOnChange, isValid} = useFormValidation({formName:'login', defaulValues: defaultValues});
+
+  const {email, password} = formValues['login'] ?? {};
+  
+  useEffect (() => { register(defaultValues);
+  },[]);
+  useEffect (() => { validate(formValues['login'] ?? {});
+  },[formValues]);
+
+  const handleOnSubmit =(e) => {
+    e.preventDefault();
+    setTimeout(() => history.push("/"), 2000);
+  };
+
   return(<>
 		<div className="card mx-auto" style={{maxWidth: '380px', marginTop:'200px'}}>
       <div className="card-body">
         <h4 className="card-title mb-4">Sign in</h4>
         {/* feedback et message d'erreurs */}
-       	<form name="login">
+       	<form name="login" onSubmit={handleOnSubmit}>
           {/* 
           <a href="#" className="btn btn-facebook btn-block mb-2"> <i className="fab fa-facebook-f"></i> &nbsp  Sign in with Facebook</a>
           <a href="#" className="btn btn-google btn-block mb-4"> <i className="fab fa-google"></i> &nbsp  Sign in with Google</a> 
           */}
           <div className="form-group" style={{ marginBottom: 0 }}>
-            <Input.Email label="Email" style={{padding: 0}} onChange={() => null}/>
+            <Input.Email label="Email" style={{padding: 0}} value={email}  onChange={handleOnChange}/>
           </div>
           <div className="form-group">
-				    <Input.Password label="Password" name="password" style={{padding: 0}}  onChange={() => null} />
+				    <Input.Password label="Password" name="password" style={{padding: 0}} value={ password}  onChange={handleOnChange} />
           </div>
           <div className="form-group"> 
 					  <Input.Checkbox col="6">Remember</Input.Checkbox>
 				  </div>   
           <div className="form-group">
-					  <Input.Submit classNamees="btn-primary btn-block" title="Login" /> 
+					  <Input.Submit classNames="btn-primary btn-block" title="Login" disable={ !isValid } /> 
 			    </div>  
       </form>
       </div>
