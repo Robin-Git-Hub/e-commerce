917ba605da9faa032a8ae3c5af880ec296a5dc71
Robin-Git-Hub <crochetrobin@gmail.com>
2021-04-13 16:32:53 +0200

    Add Route Post collections users

--- backend/server.js
+++ backend/server.js
@@ -1,66 +1,102 @@
+
 const PORT = 5000;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-
-//Use application middleware Router
-const routes = express.Router()
+const routes = express.Router();
 app.use("/api", routes);
 
-
-//Body-parser
+// body-parser
 routes.use(bodyParser.urlencoded({ extended: false }));
 routes.use(bodyParser.json());
- 
-//Cors
+const jsonParser = bodyParser.json();
+
+//cors
 routes.use(cors());
- 
-//Mango DB CLIENT
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://robin:241190@marketplaceproject.9dy5z.mongodb.net/marketplace?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const DATABASE = 'marketplace';
-// Connect to server
+
+// mongoDB client
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://robin:241190@marketplaceproject.9dy5z.mongodb.net/marketplace?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// connect to server
 app.listen(PORT, () => {
   console.log(`Server up and running on http://localhost:${PORT}`);
 });
- 
-//Routes
-routes.get("/", (req, res) => {
-  res.send("Hello World Root!");
-});
 
-// Connect to DB  
-client.connect(err => {
-  if(err) {
+// connect to DB
+const DATABASE = "marketplace";
+client.connect((err) => {
+  if (err) {
     throw Error(err);
   }
   !err && console.log(`Successfully connected to database`);
-  const products = client.db(DATABASE).collection("products");
+  const db = client.db(DATABASE);
+  const products = db.collection("products");
+  const users = db.collection("users");
 
   // perform actions on the collection object
-  routes.get("/products", (req, res) => {
-    products
-    .find()
-    .toArray()
-    .then((error, results) => {
-      if (error) { return res.send(error)}
-      res.statut(200).send({ results});
-    })
-    .catch((err) => res.send(err)); 
-  });
 
-  routes.post("/products/add", function (req, res) {
-    products.insertOne()
-    .then((error, result) => {
-      if(error) {
-        return res.send(error);
-      }
-        return res.status(200).send("successfully inserted new document");
+  // GET
+  routes.get("/products", function (req, res) {
+    products
+      .find()
+      .toArray()
+      .then((error, results) => {
+        if (error) {
+          return res.send(error);
+        }
+        res.status(200).send({ results });
       })
       .catch((err) => res.send(err));
   });
+ //route to get the user's profile with email
+ routes.get("/user/:email", function (req, res) {
+  users
+    .findOne({ email: req.params.email  }) //retrieve user profile with email
+    .then((error, results) => {
+      if (error) {
+        return res.send(error);
+      }
+      return res.status(200).send(results.data);
+    })
+    .catch((err) => res.send(err));
+});
 
-  // client.close();
+
+  // POST
+  const exampleObj = {
+    id: 29999,
+    category: "Clothes",
+    name: "Winter Jacket for Women, All sizes",
+    price: 79,
+  };
+  routes.post("/products/add", jsonParser, function (req, res) {
+    products
+      .insertOne(req.body)
+      .then(() => res.status(200).send("successfully inserted new document"))
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  });
+  routes.post("/users/add", jsonParser, function (req, res) {
+    users
+      .insertOne(req.body)
+      .then(() => res.status(200).send("successfully inserted new document"))
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  });
+});
+
+//routes
+routes.get("/", (req, res) => {
+  res.send("Hello World!");
 });
