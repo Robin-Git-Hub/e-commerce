29f58f24483b843fe76c16ac350b670161a7abcc
Robin-Git-Hub <crochetrobin@gmail.com>
2021-04-14 16:05:05 +0200

    Fix bug server Stripe endpoint checkout

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const PORT = 5000;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const routes = express.Router();
 app.use("/api", routes);
@@ -18,7 +19,7 @@ routes.use(cors());
 // mongoDB client
 const MongoClient = require("mongodb").MongoClient;
 const uri =
-  "mongodb+srv://robin:241190@marketplaceproject.9dy5z.mongodb.net/marketplace?retryWrites=true&w=majority";
+  "mongodb+srv://usertest:test123@cluster-clickcollect.j9nty.mongodb.net/marketplace?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -39,6 +40,7 @@ client.connect((err) => {
   const db = client.db(DATABASE);
   const products = db.collection("products");
   const users = db.collection("users");
+  const orders = db.collection("orders");
 
   // perform actions on the collection object
 
@@ -55,19 +57,18 @@ client.connect((err) => {
       })
       .catch((err) => res.send(err));
   });
- //route to get the user's profile with email
- routes.get("/user/:email", function (req, res) {
-  users
-    .findOne({ email: req.params.email  }) //retrieve user profile with email
-    .then((error, results) => {
-      if (error) {
-        return res.send(error);
-      }
-      return res.status(200).send(results.data);
-    })
-    .catch((err) => res.send(err));
-});
-
+  //route to get the user's profile with email
+  routes.get("/user/:email", function (req, res) {
+    users
+      .findOne({ email: req.params.email  }) //retrieve user profile with email
+      .then((error, results) => {
+        if (error) {
+          return res.send(error);
+        }
+        return res.status(200).send(results.data);
+      })
+      .catch((err) => res.send(err));
+  });
 
   // POST
   const exampleObj = {
@@ -94,39 +95,39 @@ client.connect((err) => {
         res.send(err);
       });
   });
-});
-
-//Stripe
-const stripe = require('stripe')('sk_test_51Ig7X1HoFREscj396x4na3OLteEoDR9dRu40eNGj1uYgspLiYUgntiq2kJTr1veENe6KPLJWImERAc8zZ14Oclsy00ipkXWqiA');
-const YOUR_DOMAIN = "http://localhost:3000"
-
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success`,
-    cancel_url: `${YOUR_DOMAIN}/cancel`,
+  routes.post("/orders/add", jsonParser, function (req, res) {
+    orders
+      .insertOne(req.body)
+      .then(() => res.status(200).send("successfully inserted new document"))
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
   });
-
-  res.json({ id: session.id });
 });
 
-app.listen(4242, () => console.log(`Listening on port ${4242}!`));
+//stripe
+const stripe = require("stripe")(process.env.SECRET_KEY);
+const YOUR_DOMAIN = "http://localhost:3000";
+
+routes.post("/create-checkout-session", jsonParser, async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: req.body,
+      mode: "payment",
+      success_url: `${YOUR_DOMAIN}/success`,
+      cancel_url: `${YOUR_DOMAIN}/cancel`,
+    });
+
+    res.json({ id: session.id });
+    console.log("Successfull post Stripe")
+  } catch (err) {
+    return res.status(500).send(`failed to process payment ${err}`);
+  }
+});
+
 //routes
 routes.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-
