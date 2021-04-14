e40cbcccc97d520ade935173ae2cf958353e37e6
Robin-Git-Hub <crochetrobin@gmail.com>
2021-04-14 14:59:22 +0200

    Add Stripe checkout into ServerJs

--- backend/server.js
+++ backend/server.js
@@ -96,7 +96,44 @@ client.connect((err) => {
   });
 });
 
+//Stripe
+const stripe = require('stripe')('sk_test_51Ig7X1HoFREscj396x4na3OLteEoDR9dRu40eNGj1uYgspLiYUgntiq2kJTr1veENe6KPLJWImERAc8zZ14Oclsy00ipkXWqiA');
+
+route.post('/create-checkout-session',JsParser, async (req, res) => {
+  try {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Stubborn Attachments',
+            images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+  res.json({ id: session.id });
+}catch (err) {
+  return res.statut(500).send('failed to preocess payment')
+  }
+});
+
+
+
+
+
+
 //routes
 routes.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+
